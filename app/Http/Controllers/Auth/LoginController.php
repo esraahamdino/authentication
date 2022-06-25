20cbfee3c9437e3ee36967ed388b4addb80fc542
Esraa Hamdino <soooh380@gmail.com>
2022-06-25 04:32:00 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
     /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Login Controller
+     |--------------------------------------------------------------------------
+     |
+     | This controller handles authenticating users for the application and
+     | redirecting them to your home screen. The controller uses a trait
+     | to conveniently provide its functionality to your applications.
+     |
+     */
 
     use AuthenticatesUsers;
 
@@ -36,5 +37,9 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    protected function credentials(\Illuminate\Http\Request $request)
+    {
+        return array_merge($request->only($this->username(), 'password'), ['type' => 'client']);
     }
 }
